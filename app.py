8b528795cb2efa4727369fc2605cc72460825b1f
alex <>
2024-03-01 21:50:07 -0500

    fix lat/lon

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     start_coords = (40.7244412, -73.9484945)
-    map = folium.Map(location=start_coords, zoom_start=14)
+    map = folium.Map(location=start_coords, zoom_start=15)
 
     # TODO allow adding locations with google forms
     # TODO cache the map so it is not being recomputed each time
@@ -19,8 +19,7 @@ def index():
         csv_reader = csv.reader(csvfile)
         for name, lat, lon, price in csv_reader:
             prices.append([float(lat), float(lon), float(price)])
-
-            folium.Marker([float(lon), float(lat)], popup=name).add_to(map)
+            folium.Marker([float(lat), float(lon)], popup=name).add_to(map)
 
     HeatMap(prices).add_to(map)
 
